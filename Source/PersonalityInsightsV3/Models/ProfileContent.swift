5f6b11b126b2d45278838de43fe7d1ee097e1d5b
Mike Kistler <mkistler@us.ibm.com>
2018-06-24 14:53:37 -0500

    Hand-coded changes to PIv3 for new multi-consumes

---
+++ Source/PersonalityInsightsV3/Models/ProfileContent.swift
@@ -0,0 +1,42 @@
+/**
+ * Copyright IBM Corporation 2018
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
+import Foundation
+
+/** The content for the profile request. */
+public enum ProfileContent {
+
+    case content(Content)
+    case html(String)
+    case text(String)
+
+    internal var contentType: String {
+        switch self {
+        case .content: return "application/json"
+        case .html: return "text/html"
+        case .text: return "text/plain"
+        }
+    }
+
+    internal var content: Data? {
+        switch self {
+        case .content(let content): return try? JSONEncoder().encode(content)
+        case .html(let html): return html.data(using: .utf8)
+        case .text(let text): return text.data(using: .utf8)
+        }
+    }
+
+}
